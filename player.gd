cc6d61fcb0cf285ae70b07305a429a97c648d52b
João Capucho <capucho@jcapucho.com>
2025-01-24 19:09:02 +0000

    Shoot bubbles

--- player.gd
+++ player.gd
@@ -1,9 +1,26 @@
 extends CharacterBody2D
 
-
 @export var speed: float = 300
 @export var jump_velocity: float = -400
 @export var kill_plane: float = 50
+
+@export var Bullet = preload("res://bubble.tscn")
+
+var shoot_parent: Node
+var can_fire: bool = false
+
+func _ready() -> void:
+	self.shoot_parent = get_parent()
+	if self.shoot_parent == null:
+		self.shoot_parent = self
+	
+
+func _input(event):
+	if event.is_action_pressed("shoot") and can_fire:
+		can_fire = false
+		var bullet_instance = Bullet.instantiate()
+		self.shoot_parent.add_child(bullet_instance)
+
 
 func _process(delta: float) -> void:
 	if position.y > kill_plane:
@@ -14,6 +31,8 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		can_fire = true
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
