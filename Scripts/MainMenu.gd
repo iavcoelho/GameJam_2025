8bc94e688e60d5bf7ce17490d59bba6738497063
João Capucho <capucho@jcapucho.com>
2025-01-25 22:26:23 +0000

    Add basic main menu

---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@export var start_button: Control = null
+@export var first_level: PackedScene = preload("res://Levels/Level1/stage1.tscn")
+
+func _ready():
+	start_button.grab_focus()
+
+func play_pressed() -> void:
+	get_tree().change_scene_to_packed(first_level)
+
+func options_pressed() -> void:
+	pass # Replace with function body.
+
+func quit_pressed() -> void:
+	get_tree().quit()
