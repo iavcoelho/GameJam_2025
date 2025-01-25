712ee19956340641adeda07745f336b0125afa84
Blosuhm <102238225+Blosuhm@users.noreply.github.com>
2025-01-25 00:49:49 +0000

    bubble popping animation and extras

--- Scripts/bubble.gd
+++ Scripts/bubble.gd
@@ -2,8 +2,21 @@ extends RigidBody2D
 
 class_name Bubble
 
+@onready var _animated_sprite = $AnimatedSprite2D
+
+var dying: bool = false
+
 func pop() -> void:
-	self.queue_free()
+	if dying:
+		return
+	
+	dying = true
+	
+	_animated_sprite.connect("animation_finished", self.queue_free)
+	self.freeze = true
+	self.collision_layer = 2
+	self.collision_mask = 2
+	_animated_sprite.play("pop")
 
 func _on_timer_timeout() -> void:
 	pop()
