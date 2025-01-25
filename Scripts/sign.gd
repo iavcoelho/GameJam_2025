ba50118c6fb8e95d4d97712ed5af519c03cc9b53
Sike0s <diogosantos.atb23@gmail.com>
2025-01-25 15:30:13 +0000

    improved signs

--- Scripts/sign.gd
+++ Scripts/sign.gd
@@ -1,14 +1,22 @@
 extends Node
 class_name Sign
 
+var interactable: bool = false
 @onready var _animated_sprite = $Area2D/AnimatedSprite2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is CharacterBody2D: 
 		_animated_sprite.connect("animation_finished", self.queue_free)
 		_animated_sprite.play("Arrow")
+		interactable = true
 	
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is CharacterBody2D: 
 		_animated_sprite.play("Idle")
+		interactable = true
+	
+
+func _input(event) -> void:
+	if interactable:
+		print("Text")
